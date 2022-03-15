5d07d9c210aafce47da9dc33ef2047ba92f050e6
Tajamal Iqbal <96197943+manpreet1231@users.noreply.github.com>
2022-03-15 23:39:47 +0500

    SetUpCoreData
    here i have set up the core data model where i have define the property of tittle string and discription string and then make extension needed and property file for the core data

---
+++ TODO_App/TODO_App/Model/todo_Detail.swift
@@ -0,0 +1,14 @@
+//
+//  todo_Detail.swift
+//  TODO_App
+//
+//  Created by Tajamal on 15/03/2022.
+//
+
+import Foundation
+import CoreData
+
+@objc(TodoInfo)
+public class TodoInfo: NSManagedObject{
+    
+}
