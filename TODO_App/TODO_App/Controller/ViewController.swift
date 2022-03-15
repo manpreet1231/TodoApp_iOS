4b9842cd69193426645d0979ad1376a428ba489e
Tajamal Iqbal <96197943+manpreet1231@users.noreply.github.com>
2022-03-16 00:11:15 +0500

    CoreData_implement
    Use core data to store the todo details and and i have set the attributes availble. and on popup save button press tittle and description added in the Todo List and on Swipe the Todo Item is deleted from the todo list. and when i have stop application and re_Run it and the item in Todo list i have added before already exist.This mean our local storage is working properly to manage our Todo List

--- TODO_App/TODO_App/Controller/ViewController.swift
+++ TODO_App/TODO_App/Controller/ViewController.swift
@@ -6,12 +6,13 @@
 //
 
 import UIKit
-
+import CoreData
 class ViewController: UIViewController {
     
     var ary = ["asd","sdfsdf","asdfsd","asd","sdfsdf","asdfsd"]
     //Initilization...
-    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var Detail : [TodoInfo]?
     @IBOutlet weak var txtTittle: UITextField!
     
     @IBOutlet weak var txtDescription: UITextView!
@@ -21,6 +22,20 @@ class ViewController: UIViewController {
     @IBAction func btnSave(_ sender: Any) {
         print(txtTittle.text)
         print(txtDescription.text)
+        
+        let newTodo = TodoInfo(context: self.context)
+        
+        newTodo.tittle = txtTittle.text ?? ""
+        newTodo.discription = txtDescription.text ?? ""
+        
+        do{
+            try self.context.save()
+        }
+        catch{
+            print("Eror")
+        }
+        self.fetchData()
+        
         viewAlert.isHidden = true
     }
     @IBAction func btnCancel(_ sender: Any) {
@@ -30,6 +45,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         viewAlert.layer.cornerRadius = 8
+        fetchData()
         // Do any additional setup after loading the view.
     }
 
@@ -38,24 +54,51 @@ class ViewController: UIViewController {
         viewAlert.isHidden = false
     }
     
-    
+    func fetchData(){
+        do{
+            self.Detail =  try context.fetch(TodoInfo.fetchRequest())
+            DispatchQueue.main.async {
+                self.tblTodoItemInfo.reloadData()
+            }
+            
+        }
+        catch{
+            
+        }
+    }
     
 }
 
 extension  ViewController :UITableViewDataSource,UITableViewDelegate{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return ary.count
+        return Detail?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! TodoInfoCell
         
-        cell.lblTodoTittle.text = ary[indexPath.row]
-        cell.lblTodoDetail.text = "asdfkasjdfksajdkfjaskdfj;asdjrief;asdufiewjfkdffa;kdfjapierfjkdfasjdfiefjskdfmxc vkxzjfdpapieruflkasdf;kldjfakdjfaksdjfksdjfskdjfskejfwiefjsdlkcvx.cvmx;dlkfsdjfaierskddnv"
+        cell.lblTodoTittle.text = Detail?[indexPath.row].tittle
+        cell.lblTodoDetail.text = Detail?[indexPath.row].discription
         return cell
       
     }
     
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let action = UIContextualAction(style: .destructive, title: "Delete"){ (action,view, completionHandler) in
+            
+            let todoToRemove = self.Detail?[indexPath.row]
+            self.context.delete(todoToRemove!)
+            
+            do{
+                try self.context.save()
+            }
+            catch{
+                print("Eror")
+            }
+            self.fetchData()
+    }
+        return UISwipeActionsConfiguration(actions: [action])
+    }
     
 }
